c7a551ef1b7b744dd3787aa0c583bf25cf049c1d
Alex Lecco <alexlecco24@gmail.com>
2018-03-26 20:24:44 -0300

    placeholder text added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text} from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
@@ -21,8 +21,10 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
+
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+          <Text> Placeholder </Text>
           <RootNavigation />
         </View>
       );
