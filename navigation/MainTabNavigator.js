56b9792176817ed3ee658b3567ba01bcb3695ab9
Alex Lecco <alexlecco24@gmail.com>
2018-03-26 19:24:46 -0300

    first commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+
+import Colors from '../constants/Colors';
+
+import Schedule from '../screens/Schedule';
+import MyTalks from '../screens/MyTalks';
+import Information from '../screens/Information';
+
+export default TabNavigator(
+  {
+    Cronograma: {
+      screen: Schedule,
+    },
+    Links: {
+      screen: MyTalks,
+    },
+    Information: {
+      screen: Information,
+    },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Cronograma':
+            iconName =
+              Platform.OS === 'ios'
+                ? `ios-information-circle${focused ? '' : '-outline'}`
+                : 'md-information-circle';
+            break;
+          case 'Links':
+            iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
+            break;
+          case 'Information':
+            iconName =
+              Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options';
+        }
+        return (
+          <Ionicons
+            name={iconName}
+            size={28}
+            style={{ marginBottom: -3 }}
+            color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+          />
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
