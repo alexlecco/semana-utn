dc61563bf2c141b0ec2e9c1c683368d0e2afd66d
alexlecco <alexlecco24@gmail.com>
2018-06-02 16:41:02 -0300

    react-navigator animation and swipe enabled

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -54,7 +54,7 @@ export default TabNavigator(
     }),
     tabBarComponent: TabBarBottom,
     tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false,
+    animationEnabled: true,
+    swipeEnabled: true,
   }
 );
